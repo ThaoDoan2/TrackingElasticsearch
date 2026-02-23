a7b82386161c1101906c3f19b101327c6b2c7fcc
ThaoDT <thaodt@higame.com.vn>
2026-02-23 16:13:14 +0700

    add gameplayt document

---
+++ rest-service/src/main/java/com/example/rest_service/repository/gameplay/GamePlayDocument.java
@@ -0,0 +1,160 @@
+package com.example.rest_service.repository.gameplay;
+
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Mapping;
+
+import com.example.rest_service.repository.AbstractDocument;
+
+@Document(indexName = "gameplay")
+@Mapping(mappingPath = "static/gameplay.json")
+public class GamePlayDocument extends AbstractDocument {
+
+    private String userId;
+    private String gameId;
+    private String eventType;
+
+    private String platform;
+    private String country;
+    private String gameVersion;
+    private Long highestLevel;
+    private Long loggedDay;
+    private String accountCreatedDate;
+
+    private String date;
+
+    private Long duration;
+    private String gameMode;
+    private Long gameLevel;
+    private Long difficulty;
+    private String status;
+    private Long completion;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(String gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getGameVersion() {
+        return gameVersion;
+    }
+
+    public void setGameVersion(String gameVersion) {
+        this.gameVersion = gameVersion;
+    }
+
+    public Long getHighestLevel() {
+        return highestLevel;
+    }
+
+    public void setHighestLevel(Long highestLevel) {
+        this.highestLevel = highestLevel;
+    }
+
+    public Long getLoggedDay() {
+        return loggedDay;
+    }
+
+    public void setLoggedDay(Long loggedDay) {
+        this.loggedDay = loggedDay;
+    }
+
+    public String getAccountCreatedDate() {
+        return accountCreatedDate;
+    }
+
+    public void setAccountCreatedDate(String accountCreatedDate) {
+        this.accountCreatedDate = accountCreatedDate;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Long duration) {
+        this.duration = duration;
+    }
+
+    public String getGameMode() {
+        return gameMode;
+    }
+
+    public void setGameMode(String gameMode) {
+        this.gameMode = gameMode;
+    }
+
+    public Long getGameLevel() {
+        return gameLevel;
+    }
+
+    public void setGameLevel(Long gameLevel) {
+        this.gameLevel = gameLevel;
+    }
+
+    public Long getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Long difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getCompletion() {
+        return completion;
+    }
+
+    public void setCompletion(Long completion) {
+        this.completion = completion;
+    }
+
+}
